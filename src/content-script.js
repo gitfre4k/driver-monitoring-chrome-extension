bee47ad22a87e1ddef9b4a2c0085232f55fd5b05
gitfre4k <zatocilo@gmail.com>
2025-04-02 03:13:53 +0200

    locked unfinished operations

--- src/content-script.js
+++ src/content-script.js
@@ -1,72 +1,56 @@
-setTimeout(() => {
-  console.log("yo");
+// setTimeout(() => {
+//   console.log("yo");
 
-  // const currentCompany = () =>
-  //   document.querySelector(
-  //     "#layout > div.w-full.h-screen.flex.overflow-hidden.text-primary-0.bg-buildings.bg-cover > div > header > div.flex.gap-4.items-center.justify-between.border-l.pl-4.h-full > div > button > span > span"
-  //   ).textContent;
+//   const statusEvents = document.querySelectorAll('[id^="row-"]');
+//   statusEvents.forEach((statusEvent) => {
+//     statusEvent.addEventListener("contextmenu", (event) => {
+//       const time = document.getElementById(statusEvent.id).children[3]
+//         .textContent;
+//       const place = document.getElementById(statusEvent.id).children[5]
+//         .textContent;
 
-  // chrome.runtime.onMessage.addListener(function (
-  //   request,
-  //   sender,
-  //   sendResponse
-  // ) {
-  //   if (request.action === "getCurrentCompany") {
-  //     const result = currentCompany();
-  //     sendResponse({ result: result });
-  //   }
-  // });
+//       const drivingTimeLeft = document.querySelector(
+//         "#layout > div.w-full.h-screen.flex.overflow-hidden.text-primary-0.bg-buildings.bg-cover > div > main > div > div > div > section.h-32.flex.justify-between.flex-nowrap.border-b.px-5.pb-4.pt-3 > div.flex.items-end.relative > div:nth-child(1) > svg > text:nth-child(3)"
+//       ).textContent;
 
-  const statusEvents = document.querySelectorAll('[id^="row-"]');
-  statusEvents.forEach((statusEvent) => {
-    statusEvent.addEventListener("contextmenu", (event) => {
-      const time = document.getElementById(statusEvent.id).children[3]
-        .textContent;
-      const place = document.getElementById(statusEvent.id).children[5]
-        .textContent;
+//       setTimeout(() => {
+//         if (document.getElementById("copyTextReplay") != null) return;
 
-      const drivingTimeLeft = document.querySelector(
-        "#layout > div.w-full.h-screen.flex.overflow-hidden.text-primary-0.bg-buildings.bg-cover > div > main > div > div > div > section.h-32.flex.justify-between.flex-nowrap.border-b.px-5.pb-4.pt-3 > div.flex.items-end.relative > div:nth-child(1) > svg > text:nth-child(3)"
-      ).textContent;
+//         const existingDiv = document.querySelector(
+//           "div.absolute.z-50.bg-secondary-0.shadow-xl.border.border-shade-2 > div > div"
+//         );
 
-      setTimeout(() => {
-        if (document.getElementById("copyTextReplay") != null) return;
+//         if (existingDiv) {
+//           const newDiv = document.createElement("div");
+//           newDiv.id = "copyTextReplay";
+//           newDiv.className =
+//             "hover:bg-secondary-4 border-shade-3 border-t first:border-0 text-base bg-secondary-1";
 
-        const existingDiv = document.querySelector(
-          "div.absolute.z-50.bg-secondary-0.shadow-xl.border.border-shade-2 > div > div"
-        );
+//           const innerDiv1 = document.createElement("div");
+//           innerDiv1.className = "px-4 py-1";
 
-        if (existingDiv) {
-          const newDiv = document.createElement("div");
-          newDiv.id = "copyTextReplay";
-          newDiv.className =
-            "hover:bg-secondary-4 border-shade-3 border-t first:border-0 text-base bg-secondary-1";
+//           const innerDiv2 = document.createElement("div");
+//           innerDiv2.className = "w-full cursor-pointer";
+//           innerDiv2.textContent = "Copy time and location for text replay";
 
-          const innerDiv1 = document.createElement("div");
-          innerDiv1.className = "px-4 py-1";
+//           existingDiv.appendChild(newDiv);
+//           newDiv.appendChild(innerDiv1);
+//           innerDiv1.appendChild(innerDiv2);
 
-          const innerDiv2 = document.createElement("div");
-          innerDiv2.className = "w-full cursor-pointer";
-          innerDiv2.textContent = "Copy time and location for text replay";
-
-          existingDiv.appendChild(newDiv);
-          newDiv.appendChild(innerDiv1);
-          innerDiv1.appendChild(innerDiv2);
-
-          newDiv.addEventListener("click", () => {
-            navigator.clipboard.writeText(
-              `This shift began at ${time}, location ${
-                place.split(" ")[2] +
-                " " +
-                place.split(" ")[3] +
-                (place.split(" ")[4] ? " " + place.split(" ")[4] : "") +
-                (place.split(" ")[5] ? " " + place.split(" ")[5] : "")
-              }. As of the current time, ${drivingTimeLeft} hours remain for driving. ` +
-                `Please ensure that logs for previous days are certified and that your trailer ID and BOL number are up-to-date.`
-            );
-          });
-        }
-      }, 300);
-    });
-  });
-}, 1500);
+//           newDiv.addEventListener("click", () => {
+//             navigator.clipboard.writeText(
+//               `This shift began at ${time}, location ${
+//                 place.split(" ")[2] +
+//                 " " +
+//                 place.split(" ")[3] +
+//                 (place.split(" ")[4] ? " " + place.split(" ")[4] : "") +
+//                 (place.split(" ")[5] ? " " + place.split(" ")[5] : "")
+//               }. As of the current time, ${drivingTimeLeft} hours remain for driving. ` +
+//                 `Please ensure that logs for previous days are certified and that your trailer ID and BOL number are up-to-date.`
+//             );
+//           });
+//         }
+//       }, 300);
+//     });
+//   });
+// }, 1500);
