5b44348d6ad6e0eb1c0ad3c1d85b7f62aa4bc490
gitfre4k <zatocilo@gmail.com>
2025-08-25 19:45:13 +0200

    [dateService] reborn

--- src/app/components/monitor/monitor.component.ts
+++ src/app/components/monitor/monitor.component.ts
@@ -2,7 +2,7 @@ import { Component, inject } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { MonitorService } from '../../@services/monitor.service';
-import { MatIconModule } from '@angular/material/icon';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -40,5 +40,39 @@ export class MonitorComponent {
 
   getNoSpaceNote(note: string) {
     return note.replace(/\s/g, '');
+  }
+
+  formatTenantName(tenant: string) {
+    const keywordsToRemove = new Set([
+      'logistics',
+      'transport',
+      'transportations',
+      'express',
+      'enterprises',
+      'enterprise',
+      'freight',
+      'international',
+      'cargo',
+      'services',
+      'trucking',
+      'systems',
+      'transporting',
+    ]);
+    let words = tenant.replace(/,/g, '').trim().split(' ');
+
+    if (words.length === 0) return '';
+
+    let lastWord = words[words.length - 1];
+    if (
+      lastWord.length === 3 ||
+      (lastWord.length === 4 && lastWord[lastWord.length - 1] === '.')
+    )
+      words.pop();
+    if (words.length > 0) {
+      const newLastWord = words[words.length - 1];
+      if (keywordsToRemove.has(newLastWord.toLowerCase())) words.pop();
+    }
+
+    return words.join(' ');
   }
 }
