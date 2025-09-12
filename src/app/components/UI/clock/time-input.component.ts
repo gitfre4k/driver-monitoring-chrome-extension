e30b060328c41e5efc7f9cd43e53fb2cde149cbc
gitfre4k <zatocilo@gmail.com>
2025-09-12 21:47:28 +0200

    dondesta mi horas pendeho

--- src/app/components/UI/clock/time-input.component.ts
+++ src/app/components/UI/clock/time-input.component.ts
@@ -100,153 +100,173 @@ export class TimeInputComponent implements OnInit, OnDestroy {
         break;
     }
   }
-
   onMouseWheel(event: WheelEvent, controlName: TInputTime) {
     event.preventDefault();
 
     let hours: number = +this.timeForm.get('hours')?.value;
     let minutes: number = +this.timeForm.get('minutes')?.value;
+    let seconds: number = +this.timeForm.get('seconds')?.value;
     let period: 'AM' | 'PM' = this.timeForm.get('period')?.value;
-    const isClockMode = this.fullClock();
 
-    let value: string = this.timeForm.get(controlName)?.value;
+    const isClockMode: boolean = this.fullClock();
+    let currentValue: number = +this.timeForm.get(controlName)?.value;
 
-    if (event.deltaY < 0) {
-      if (controlName === 'period') value = 'PM';
-      // positive +++
-      if (controlName === 'hours') {
-        if (+value === (isClockMode ? 11 : 99)) {
-          if (period === 'PM') {
-            value = '11';
+    const isScrollUp = event.deltaY < 0; // true for up, false for down
+
+    switch (controlName) {
+      case 'hours':
+        if (isScrollUp) {
+          hours++;
+          if (isClockMode) {
+            if (hours > 12) {
+              hours = 1;
+              if (period) this.togglePeriod(period);
+            }
           } else {
-            value = '99';
+            if (hours > 99) hours = 0;
           }
-        } else if (period === 'AM' && hours === 12) {
-          value = '01';
-          this.timeForm.get('period')?.patchValue('PM');
-        } else value = (+value + 1).toFixed();
-      }
-      if (controlName === 'minutes') {
-        if (+value === 59) {
-          if (hours < (isClockMode ? 11 : 99)) {
-            let newHours = `${hours + 1 < 10 ? '0' : ''}${hours + 1}`;
-            value = '00';
-            this.timeForm.get('hours')?.patchValue(newHours);
+        } else {
+          // Scroll down
+          hours--;
+          if (isClockMode) {
+            if (hours < 1) {
+              hours = 12;
+              if (period) this.togglePeriod(period);
+            }
           } else {
-            if (period === 'AM') {
-              value = '00';
-              this.timeForm.get('hours')?.patchValue('01');
-              this.timeForm.get('period')?.patchValue('PM');
-            } else {
-              value = '59';
+            if (hours < 0) hours = 99;
+          }
+        }
+        this.timeForm.get(controlName)?.patchValue(this.formatValue(hours));
+        break;
+
+      case 'minutes':
+        if (isScrollUp) {
+          minutes++;
+          if (minutes > 59) {
+            minutes = 0;
+            this.timeForm
+              .get('hours')
+              ?.patchValue(
+                this.formatValue(
+                  isClockMode
+                    ? this.incrementHour(hours, period)
+                    : this.incrementHour(hours),
+                ),
+              );
+            if (isClockMode && period) this.togglePeriod(period);
+          }
+        } else {
+          // Scroll down
+          minutes--;
+          if (minutes < 0) {
+            minutes = 59;
+            this.timeForm
+              .get('hours')
+              ?.patchValue(
+                this.formatValue(
+                  isClockMode
+                    ? this.decrementHour(hours, period)
+                    : this.decrementHour(hours),
+                ),
+              );
+            if (isClockMode && period) this.togglePeriod(period);
+          }
+        }
+        this.timeForm.get(controlName)?.patchValue(this.formatValue(minutes));
+        break;
+
+      case 'seconds':
+        if (isScrollUp) {
+          seconds++;
+          if (seconds > 59) {
+            seconds = 0;
+            this.timeForm
+              .get('minutes')
+              ?.patchValue(this.formatValue(this.incrementMinute(minutes)));
+            if (minutes + 1 > 59) {
+              this.timeForm
+                .get('hours')
+                ?.patchValue(
+                  this.formatValue(
+                    isClockMode
+                      ? this.incrementHour(hours, period)
+                      : this.incrementHour(hours),
+                  ),
+                );
+              if (isClockMode && period) this.togglePeriod(period);
             }
           }
         } else {
-          value = (+value + 1).toFixed();
-        }
-      }
-
-      if (controlName === 'seconds') {
-        if (+value === 59) {
-          if (minutes === 59) {
-            if (hours === (isClockMode ? 11 : 99)) {
-              if (period && period === 'AM') {
-                value = '00';
-                this.timeForm.get('minutes')?.patchValue('00');
-                this.timeForm.get('hours')?.patchValue('01');
-                this.timeForm.get('period')?.patchValue('PM');
-              } else {
-                value = '59';
-              }
-            } else {
-              value = '00';
-              let newHours = `${hours + 1 < 10 ? '0' : ''}${hours + 1}`;
-              this.timeForm.get('hours')?.patchValue(newHours);
-              this.timeForm.get('minutes')?.patchValue('00');
+          // Scroll down
+          seconds--;
+          if (seconds < 0) {
+            seconds = 59;
+            this.timeForm
+              .get('minutes')
+              ?.patchValue(this.formatValue(this.decrementMinute(minutes)));
+            if (minutes - 1 < 0) {
+              this.timeForm
+                .get('hours')
+                ?.patchValue(
+                  this.formatValue(
+                    isClockMode
+                      ? this.decrementHour(hours, period)
+                      : this.decrementHour(hours),
+                  ),
+                );
+              if (isClockMode && period) this.togglePeriod(period);
             }
-          } else {
-            let newMinutes = `${minutes + 1 < 10 ? '0' : ''}${minutes + 1}`;
-            this.timeForm.get('minutes')?.patchValue(newMinutes);
-            value = '00';
           }
-        } else {
-          value = (+value + 1).toFixed();
         }
-      }
+        this.timeForm.get(controlName)?.patchValue(this.formatValue(seconds));
+        break;
+
+      case 'period':
+        if (period) {
+          this.togglePeriod(period, isScrollUp);
+        }
+        break;
     }
-    if (event.deltaY > 0) {
-      if (controlName === 'period') value = 'AM';
-      // negative ---
+  }
 
-      if (controlName === 'hours') {
-        if (hours === 0) value = '00';
-        else value = (+value - 1).toFixed();
+  private formatValue(value: number): string {
+    return value < 10 ? `0${value}` : `${value}`;
+  }
 
-        if (period === 'PM') {
-          if (hours === 12) this.timeForm.get('period')?.patchValue('AM');
-          if (hours === 1) value = '12';
-        }
+  private togglePeriod(currentPeriod: 'AM' | 'PM', isScrollUp?: boolean): void {
+    const newPeriod = currentPeriod === 'AM' ? 'PM' : 'AM';
+    this.timeForm.get('period')?.patchValue(newPeriod);
+  }
+
+  private incrementHour(hours: number, period?: 'AM' | 'PM'): number {
+    if (period) {
+      if (hours === 11) {
+        if (period === 'AM') this.togglePeriod('AM');
+        else this.togglePeriod('PM');
       }
-      if (controlName === 'minutes') {
-        if (+value === 0) {
-          value = '59';
-          if (hours !== 0) {
-            let newHours = `${hours - 1 < 10 ? '0' : ''}${hours - 1}`;
-            this.timeForm.get('hours')?.patchValue(newHours);
-          }
-          if (period === 'PM') {
-            if (hours === 12) {
-              this.timeForm.get('period')?.patchValue('AM');
-            }
-            if (hours === 1) {
-              this.timeForm.get('hours')?.patchValue('12');
-            }
-          }
-        } else {
-          value = (+value - 1).toFixed();
-        }
-        if (hours === 0 && +value === 0) {
-          value = '01';
-        }
-      }
-
-      if (controlName === 'seconds') {
-        if (+value === 0) {
-          value = '59';
-
-          if (minutes !== 0) {
-            let newMinutes = `${minutes - 1 < 10 ? '0' : ''}${minutes - 1}`;
-            this.timeForm.get('minutes')?.patchValue(newMinutes);
-          } else {
-            if (hours !== 0) {
-              this.timeForm.get('minutes')?.patchValue('59');
-              let newHours = `${hours - 1 < 10 ? '0' : ''}${hours - 1}`;
-              this.timeForm.get('hours')?.patchValue(newHours);
-            }
-            if (period === 'PM') {
-              if (hours === 12) {
-                this.timeForm.get('period')?.patchValue('AM');
-                this.timeForm.get('hours')?.patchValue('11');
-              }
-              if (hours === 1) {
-                this.timeForm.get('hours')?.patchValue('12');
-              }
-            } else {
-              value = '01';
-            }
-          }
-        } else {
-          value = (+value - 1).toFixed();
-        }
-        if (hours === 0 && minutes === 0 && +value === 0) {
-          value = '01';
-        }
-      }
+      if (hours === 12) return 1;
     }
-    if (+value > 0 && +value < 10) value = '0' + +value;
-    if (+value <= 0) value = '00';
+    return (hours + 1) % 100;
+  }
 
-    this.timeForm.get(controlName)?.patchValue(value);
+  private decrementHour(hours: number, period?: 'AM' | 'PM'): number {
+    if (period) {
+      if (hours === 12) {
+        if (period === 'AM') this.togglePeriod('AM');
+        else this.togglePeriod('PM');
+      }
+      if (hours === 1) return 12;
+    }
+    return hours - 1 < 0 ? 99 : hours - 1;
+  }
+
+  private incrementMinute(minutes: number): number {
+    return (minutes + 1) % 60;
+  }
+
+  private decrementMinute(minutes: number): number {
+    return minutes - 1 < 0 ? 59 : minutes - 1;
   }
 
   ngAfterViewInit(): void {
