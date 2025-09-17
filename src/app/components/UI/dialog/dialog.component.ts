7da730b3fede728ea6b5e76f769d2559c0133a77
unknown <bogdan.dimitrijevic992@gmail.com>
2025-09-17 01:44:47 -0400

    shift n shit

--- src/app/components/UI/dialog/dialog.component.ts
+++ src/app/components/UI/dialog/dialog.component.ts
@@ -1,21 +1,22 @@
-import { Component, computed, inject, signal } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { MatDialogRef } from "@angular/material/dialog";
-import { ShiftPeriodComponent } from "../shift-period/shift-period.component";
-import { MatButtonModule } from "@angular/material/button";
-import { MatSelectModule } from "@angular/material/select";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatIconModule } from "@angular/material/icon";
+import { Component, computed, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { ShiftPeriodComponent } from '../shift-period/shift-period.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import {
   IHoursOutput,
   IMinutesOutput,
-} from "../../../interfaces/api.interface";
-import { ContextMenuService } from "../../../@services/context-menu.service";
-import { MonitorService } from "../../../@services/monitor.service";
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+} from '../../../interfaces/api.interface';
+import { ContextMenuService } from '../../../@services/context-menu.service';
+import { MonitorService } from '../../../@services/monitor.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { DurationPipe } from '../../../pipes/duration.pipe';
 
 @Component({
-  selector: "app-dialog",
+  selector: 'app-dialog',
   imports: [
     FormsModule,
     ShiftPeriodComponent,
@@ -24,22 +25,67 @@ import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
     MatFormFieldModule,
     MatIconModule,
     MatProgressSpinnerModule,
+    DurationPipe,
   ],
-  templateUrl: "./dialog.component.html",
-  styleUrl: "./dialog.component.scss",
+  templateUrl: './dialog.component.html',
+  styleUrl: './dialog.component.scss',
 })
 export class DialogComponent {
   readonly dialogRef = inject(MatDialogRef<DialogComponent>);
   contextMenuService = inject(ContextMenuService);
   monitorService = inject(MonitorService);
 
-  direction = signal<"Past" | "Future">("Future");
-  hh = signal("");
-  mm = signal("");
+  direction = signal<'Past' | 'Future'>('Future');
+  hh = signal('');
+  mm = signal('');
   time = computed(() => {
     const hh = this.hh();
     const mm = this.mm();
     return `${hh}:${mm}`;
+  });
+
+  dutyStatusNames = [
+    'On Duty',
+    'Sleeper Berth',
+    'Off Duty',
+    'Driving',
+    'PC',
+    'YM',
+  ];
+
+  space = computed(() => {
+    const selectedEvents = this.monitorService
+      .selectedEvents()
+      .sort((a, b) => a.computeIndex - b.computeIndex);
+    const logEvents = this.monitorService.computedDailyLogEvents();
+    ////////////////////
+    if (!logEvents) return null;
+
+    let backward = 0;
+    let forward = 0;
+
+    for (let i = selectedEvents[0].computeIndex - 1; i >= 0; i--) {
+      if (this.dutyStatusNames.includes(logEvents[i].statusName)) {
+        backward = logEvents[i].realDurationInSeconds;
+        break;
+      }
+
+      // 6
+      //// 0 1 2 3 4 5
+    }
+
+    for (
+      let i = selectedEvents[selectedEvents.length - 1].computeIndex;
+      i < logEvents.length;
+      i++
+    ) {
+      if (this.dutyStatusNames.includes(logEvents[i].statusName)) {
+        forward =
+          logEvents[i].realDurationInSeconds ?? logEvents[i].durationInSeconds;
+        break;
+      }
+    }
+    return { backward, forward };
   });
 
   ngOnInit() {
@@ -51,7 +97,7 @@ export class DialogComponent {
     event.preventDefault();
 
     !this.monitorService.isShifting() &&
-      this.direction.set(event.deltaY > 0 ? "Past" : "Future");
+      this.direction.set(event.deltaY > 0 ? 'Past' : 'Future');
   }
 
   handleHoursInputChange(event: IHoursOutput) {
@@ -64,7 +110,7 @@ export class DialogComponent {
 
   onShift() {
     this.contextMenuService.handleMultiEventAction(
-      "SHIFT_EVENTS",
+      'SHIFT_EVENTS',
       this.monitorService.selectedEvents(),
       {
         direction: this.direction(),
--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -1,98 +1,106 @@
 export type TScanMode =
-  | "violations"
-  | "dot"
-  | "advanced"
-  | "pre"
-  | "cert"
-  | "deleteUE";
+  | 'violations'
+  | 'dot'
+  | 'advanced'
+  | 'pre'
+  | 'cert'
+  | 'deleteUE';
 
-export type TErrorParsedComparison = "smaller" | "greater";
+export type TErrorParsedComparison = 'smaller' | 'greater';
 
 export type TScanResult =
-  | "teleports"
-  | "locationMismatch"
-  | "eventErrors"
-  | "prolongedOnDuty"
-  | "malfOrDataDiag"
-  | "pcYm"
-  | "missingEngineOn"
-  | "manualDriving"
-  | "highEngineHours"
-  | "lowTotalEngineHours"
-  | "newDrivers"
-  | "fleetManager"
-  | "preViolations"
-  | "cycleHours"
-  | "refuelWarning"
-  | "truckChange"
-  | "certStatus";
+  | 'teleports'
+  | 'locationMismatch'
+  | 'eventErrors'
+  | 'prolongedOnDuty'
+  | 'malfOrDataDiag'
+  | 'pcYm'
+  | 'missingEngineOn'
+  | 'manualDriving'
+  | 'highEngineHours'
+  | 'lowTotalEngineHours'
+  | 'newDrivers'
+  | 'fleetManager'
+  | 'preViolations'
+  | 'cycleHours'
+  | 'refuelWarning'
+  | 'truckChange'
+  | 'certStatus';
 
 export type TProgressMode =
-  | "determinate"
-  | "indeterminate"
-  | "buffer"
-  | "query";
+  | 'determinate'
+  | 'indeterminate'
+  | 'buffer'
+  | 'query';
 
 export type TContextMenuAction =
-  | "EXTEND_PTI"
-  | "ADD_PTI"
-  | "DELETE_ALL_ENGINES"
-  | "DELETE_DRIVING_ENGINES"
-  | "DELETE_ENGINES_IN_DRIVING"
-  | "ADD_ENGINE_OFF"
-  | "UPDATE_EVENT"
-  | "PARTIAL_ON_TO_OFF"
-  | "PARTIAL_ON_TO_SLEEP"
-  | "PARTIAL_TO_ON"
-  | "RESIZE"
-  | "ADVANCED_RESIZE"
-  | "SHIFT_EVENTS"
-  | "DELETE_SELECTED_EVENTS"
-  | "DUPLICATE"
-  | "COPY_LOCATION"
-  | "PASTE_LOCATION"
-  | "ChangeToOffDutyStatus"
-  | "ChangeToSleeperBerthStatus"
-  | "ChangeToDrivingStatus"
-  | "ChangeToOnDutyNotDrivingStatus";
+  | 'EXTEND_PTI'
+  | 'ADD_PTI'
+  | 'DELETE_ALL_ENGINES'
+  | 'DELETE_DRIVING_ENGINES'
+  | 'DELETE_ENGINES_IN_DRIVING'
+  | 'ADD_ENGINE_OFF'
+  | 'UPDATE_EVENT'
+  | 'PARTIAL_ON_TO_OFF'
+  | 'PARTIAL_ON_TO_SLEEP'
+  | 'PARTIAL_TO_ON'
+  | 'RESIZE'
+  | 'ADVANCED_RESIZE'
+  | 'SHIFT_EVENTS'
+  | 'DELETE_SELECTED_EVENTS'
+  | 'DUPLICATE'
+  | 'COPY_LOCATION'
+  | 'PASTE_LOCATION'
+  | 'ChangeToOffDutyStatus'
+  | 'ChangeToSleeperBerthStatus'
+  | 'ChangeToDrivingStatus'
+  | 'ChangeToOnDutyNotDrivingStatus';
 
 export type TFocusElementAction =
-  | "FOCUS_TACHOGRAPH_START"
-  | "FOCUS_TACHOGRAPH_STOP"
-  | "ELEMENT_ON_CLICK";
+  | 'FOCUS_TACHOGRAPH_START'
+  | 'FOCUS_TACHOGRAPH_STOP'
+  | 'ELEMENT_ON_CLICK';
 
 export type TEventTypeCode =
-  | "ChangeToOffDutyStatus"
-  | "ChangeToSleeperBerthStatus"
-  | "ChangeToDrivingStatus"
-  | "ChangeToOnDutyNotDrivingStatus"
-  | "IntermediateLogConventionalLocationPrecision"
-  | "IntermediateLogReducedLocationPrecision"
-  | "DriversFirstCertification"
-  | "DriversSecondCertification"
-  | "DriversThirdCertification"
-  | "DriversFourthCertification"
-  | "DriversFifthCertification"
-  | "DriversSixthCertification"
-  | "DriversSeventhCertification"
-  | "DriversEighthCertification"
-  | "DriversNinthCertification"
-  | "AuthenticatedDriverLogin"
-  | "AuthenticatedDriverLogout"
-  | "EnginePowerUpConventionalLocationPrecision"
-  | "EnginePowerUpReducedLocationPrecision"
-  | "EngineShutDownConventionalLocationPrecision"
-  | "EngineShutDownReducedLocationPrecision"
-  | "Remark"
-  | "EldConnected"
-  | "EldDisconnected"
-  | "CoDriverAdd"
-  | "CoDriverRemove"
-  | "TrailerAttach"
-  | "TrailerDetach"
-  | "VehicleSelect"
-  | "DriversDeCertificationOfRecords"
-  | "VehicleStartOfDay"
-  | "Dvir";
+  | 'ChangeToOffDutyStatus'
+  | 'ChangeToSleeperBerthStatus'
+  | 'ChangeToDrivingStatus'
+  | 'ChangeToOnDutyNotDrivingStatus'
+  | 'IntermediateLogConventionalLocationPrecision'
+  | 'IntermediateLogReducedLocationPrecision'
+  | 'DriversFirstCertification'
+  | 'DriversSecondCertification'
+  | 'DriversThirdCertification'
+  | 'DriversFourthCertification'
+  | 'DriversFifthCertification'
+  | 'DriversSixthCertification'
+  | 'DriversSeventhCertification'
+  | 'DriversEighthCertification'
+  | 'DriversNinthCertification'
+  | 'AuthenticatedDriverLogin'
+  | 'AuthenticatedDriverLogout'
+  | 'EnginePowerUpConventionalLocationPrecision'
+  | 'EnginePowerUpReducedLocationPrecision'
+  | 'EngineShutDownConventionalLocationPrecision'
+  | 'EngineShutDownReducedLocationPrecision'
+  | 'Remark'
+  | 'EldConnected'
+  | 'EldDisconnected'
+  | 'CoDriverAdd'
+  | 'CoDriverRemove'
+  | 'TrailerAttach'
+  | 'TrailerDetach'
+  | 'VehicleSelect'
+  | 'DriversDeCertificationOfRecords'
+  | 'VehicleStartOfDay'
+  | 'Dvir';
 
-export type TInputTime = "hours" | "minutes" | "seconds" | "period";
+export type TInputTime = 'hours' | 'minutes' | 'seconds' | 'period';
+
+export type TDutyStatusName =
+  | 'On Duty'
+  | 'Sleeper Berth'
+  | 'Off Duty'
+  | 'Driving'
+  | 'PC'
+  | 'YM';
