7f777366432b251e8fc5f752a5bdfc0dfcf1dd71
unknown <bogdan.dimitrijevic992@gmail.com>
2025-10-09 00:13:47 -0400

    0.0.4.12

--- src/app/@services/constants.service.ts
+++ src/app/@services/constants.service.ts
@@ -5,5 +5,5 @@ import { Injectable, signal } from '@angular/core';
 })
 export class ConstantsService {
   httpLimit = signal(8);
-  extensionVersion = signal('0.0.4.11');
+  extensionVersion = signal('0.0.4.12');
 }
